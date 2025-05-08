4aff3274d6aa7da380a0d82fb5a09cf835ccdf72
Arie Akbarull Ridho <arieinjector@gmail.com>
2025-05-08 12:07:11 +0700

    init

---
+++ Views/Dashboard/DashboardMainForm.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace NutriNyan.Views.Dashboard
+{
+    partial class DashboardMainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            sidebarMenu = new Panel();
+            settingButton = new FontAwesome.Sharp.IconButton();
+            reminderButton = new FontAwesome.Sharp.IconButton();
+            konsultasiButton = new FontAwesome.Sharp.IconButton();
+            TrackingAirButton = new FontAwesome.Sharp.IconButton();
+            TrackingGiziButton = new FontAwesome.Sharp.IconButton();
+            dashboardButton = new FontAwesome.Sharp.IconButton();
+            panelLogo = new Panel();
+            logoApp = new PictureBox();
+            panelContent = new Panel();
+            sidebarMenu.SuspendLayout();
+            panelLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)logoApp).BeginInit();
+            SuspendLayout();
+            // 
+            // sidebarMenu
+            // 
+            sidebarMenu.BackColor = Color.FromArgb(1, 54, 63);
+            sidebarMenu.Controls.Add(settingButton);
+            sidebarMenu.Controls.Add(reminderButton);
+            sidebarMenu.Controls.Add(konsultasiButton);
+            sidebarMenu.Controls.Add(TrackingAirButton);
+            sidebarMenu.Controls.Add(TrackingGiziButton);
+            sidebarMenu.Controls.Add(dashboardButton);
+            sidebarMenu.Controls.Add(panelLogo);
+            sidebarMenu.Dock = DockStyle.Left;
+            sidebarMenu.Location = new Point(0, 0);
+            sidebarMenu.Name = "sidebarMenu";
+            sidebarMenu.Size = new Size(262, 709);
+            sidebarMenu.TabIndex = 0;
+            // 
+            // settingButton
+            // 
+            settingButton.Dock = DockStyle.Top;
+            settingButton.FlatAppearance.BorderSize = 0;
+            settingButton.FlatStyle = FlatStyle.Flat;
+            settingButton.Font = new Font("Segoe UI Semibold", 12.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            settingButton.ForeColor = Color.FromArgb(183, 201, 200);
+            settingButton.IconChar = FontAwesome.Sharp.IconChar.Cog;
+            settingButton.IconColor = Color.FromArgb(183, 201, 200);
+            settingButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            settingButton.IconSize = 36;
+            settingButton.ImageAlign = ContentAlignment.MiddleLeft;
+            settingButton.Location = new Point(0, 440);
+            settingButton.Name = "settingButton";
+            settingButton.Padding = new Padding(10, 0, 15, 0);
+            settingButton.Size = new Size(262, 60);
+            settingButton.TabIndex = 6;
+            settingButton.Text = "Pengaturan";
+            settingButton.TextAlign = ContentAlignment.MiddleLeft;
+            settingButton.TextImageRelation = TextImageRelation.ImageBeforeText;
+            settingButton.UseVisualStyleBackColor = true;
+            // 
+            // reminderButton
+            // 
+            reminderButton.Dock = DockStyle.Top;
+            reminderButton.FlatAppearance.BorderSize = 0;
+            reminderButton.FlatStyle = FlatStyle.Flat;
+            reminderButton.Font = new Font("Segoe UI Semibold", 12.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            reminderButton.ForeColor = Color.FromArgb(183, 201, 200);
+            reminderButton.IconChar = FontAwesome.Sharp.IconChar.Bell;
+            reminderButton.IconColor = Color.FromArgb(183, 201, 200);
+            reminderButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            reminderButton.IconSize = 36;
+            reminderButton.ImageAlign = ContentAlignment.MiddleLeft;
+            reminderButton.Location = new Point(0, 380);
+            reminderButton.Name = "reminderButton";
+            reminderButton.Padding = new Padding(10, 0, 15, 0);
+            reminderButton.Size = new Size(262, 60);
+            reminderButton.TabIndex = 5;
+            reminderButton.Text = "Pengingat Makan";
+            reminderButton.TextAlign = ContentAlignment.MiddleLeft;
+            reminderButton.TextImageRelation = TextImageRelation.ImageBeforeText;
+            reminderButton.UseVisualStyleBackColor = true;
+            // 
+            // konsultasiButton
+            // 
+            konsultasiButton.Dock = DockStyle.Top;
+            konsultasiButton.FlatAppearance.BorderSize = 0;
+            konsultasiButton.FlatStyle = FlatStyle.Flat;
+            konsultasiButton.Font = new Font("Segoe UI Semibold", 12.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            konsultasiButton.ForeColor = Color.FromArgb(183, 201, 200);
+            konsultasiButton.IconChar = FontAwesome.Sharp.IconChar.Comments;
+            konsultasiButton.IconColor = Color.FromArgb(183, 201, 200);
+            konsultasiButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            konsultasiButton.IconSize = 36;
+            konsultasiButton.ImageAlign = ContentAlignment.MiddleLeft;
+            konsultasiButton.Location = new Point(0, 320);
+            konsultasiButton.Name = "konsultasiButton";
+            konsultasiButton.Padding = new Padding(10, 0, 15, 0);
+            konsultasiButton.Size = new Size(262, 60);
+            konsultasiButton.TabIndex = 4;
+            konsultasiButton.Text = "Konsultasi Gizi";
+            konsultasiButton.TextAlign = ContentAlignment.MiddleLeft;
+            konsultasiButton.TextImageRelation = TextImageRelation.ImageBeforeText;
+            konsultasiButton.UseVisualStyleBackColor = true;
+            // 
+            // TrackingAirButton
+            // 
+            TrackingAirButton.Dock = DockStyle.Top;
+            TrackingAirButton.FlatAppearance.BorderSize = 0;
+            TrackingAirButton.FlatStyle = FlatStyle.Flat;
+            TrackingAirButton.Font = new Font("Segoe UI Semibold", 12.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            TrackingAirButton.ForeColor = Color.FromArgb(183, 201, 200);
+            TrackingAirButton.IconChar = FontAwesome.Sharp.IconChar.GlassWaterDroplet;
+            TrackingAirButton.IconColor = Color.FromArgb(183, 201, 200);
+            TrackingAirButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            TrackingAirButton.IconSize = 36;
+            TrackingAirButton.ImageAlign = ContentAlignment.MiddleLeft;
+            TrackingAirButton.Location = new Point(0, 260);
+            TrackingAirButton.Name = "TrackingAirButton";
+            TrackingAirButton.Padding = new Padding(10, 0, 15, 0);
+            TrackingAirButton.Size = new Size(262, 60);
+            TrackingAirButton.TabIndex = 3;
+            TrackingAirButton.Text = "Tracking Air";
+            TrackingAirButton.TextAlign = ContentAlignment.MiddleLeft;
+            TrackingAirButton.TextImageRelation = TextImageRelation.ImageBeforeText;
+            TrackingAirButton.UseVisualStyleBackColor = true;
+            // 
+            // TrackingGiziButton
+            // 
+            TrackingGiziButton.Dock = DockStyle.Top;
+            TrackingGiziButton.FlatAppearance.BorderSize = 0;
+            TrackingGiziButton.FlatStyle = FlatStyle.Flat;
+            TrackingGiziButton.Font = new Font("Segoe UI Semibold", 12.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            TrackingGiziButton.ForeColor = Color.FromArgb(183, 201, 200);
+            TrackingGiziButton.IconChar = FontAwesome.Sharp.IconChar.Utensils;
+            TrackingGiziButton.IconColor = Color.FromArgb(183, 201, 200);
+            TrackingGiziButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            TrackingGiziButton.IconSize = 36;
+            TrackingGiziButton.ImageAlign = ContentAlignment.MiddleLeft;
+            TrackingGiziButton.Location = new Point(0, 200);
+            TrackingGiziButton.Name = "TrackingGiziButton";
+            TrackingGiziButton.Padding = new Padding(10, 0, 15, 0);
+            TrackingGiziButton.Size = new Size(262, 60);
+            TrackingGiziButton.TabIndex = 2;
+            TrackingGiziButton.Text = "Tracking Gizi";
+            TrackingGiziButton.TextAlign = ContentAlignment.MiddleLeft;
+            TrackingGiziButton.TextImageRelation = TextImageRelation.ImageBeforeText;
+            TrackingGiziButton.UseVisualStyleBackColor = true;
+            TrackingGiziButton.Click += TrackingGiziButton_Click;
+            // 
+            // dashboardButton
+            // 
+            dashboardButton.Dock = DockStyle.Top;
+            dashboardButton.FlatAppearance.BorderSize = 0;
+            dashboardButton.FlatStyle = FlatStyle.Flat;
+            dashboardButton.Font = new Font("Segoe UI Semibold", 12.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            dashboardButton.ForeColor = Color.FromArgb(183, 201, 200);
+            dashboardButton.IconChar = FontAwesome.Sharp.IconChar.House;
+            dashboardButton.IconColor = Color.FromArgb(183, 201, 200);
+            dashboardButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            dashboardButton.IconSize = 36;
+            dashboardButton.ImageAlign = ContentAlignment.MiddleLeft;
+            dashboardButton.Location = new Point(0, 140);
+            dashboardButton.Name = "dashboardButton";
+            dashboardButton.Padding = new Padding(10, 0, 15, 0);
+            dashboardButton.Size = new Size(262, 60);
+            dashboardButton.TabIndex = 1;
+            dashboardButton.Text = "Dashboard";
+            dashboardButton.TextAlign = ContentAlignment.MiddleLeft;
+            dashboardButton.TextImageRelation = TextImageRelation.ImageBeforeText;
+            dashboardButton.UseVisualStyleBackColor = true;
+            dashboardButton.Click += dashboardButton_Click;
+            // 
+            // panelLogo
+            // 
+            panelLogo.Controls.Add(logoApp);
+            panelLogo.Dock = DockStyle.Top;
+            panelLogo.Location = new Point(0, 0);
+            panelLogo.Name = "panelLogo";
+            panelLogo.Size = new Size(262, 140);
+            panelLogo.TabIndex = 0;
+            // 
+            // logoApp
+            // 
+            logoApp.Image = Properties.Resources.nutrinyan_logo;
+            logoApp.Location = new Point(56, 36);
+            logoApp.Name = "logoApp";
+            logoApp.Size = new Size(139, 70);
+            logoApp.SizeMode = PictureBoxSizeMode.Zoom;
+            logoApp.TabIndex = 1;
+            logoApp.TabStop = false;
+            // 
+            // panelContent
+            // 
+            panelContent.BackColor = Color.FromArgb(17, 30, 29);
+            panelContent.Dock = DockStyle.Fill;
+            panelContent.Location = new Point(262, 0);
+            panelContent.Name = "panelContent";
+            panelContent.Size = new Size(985, 709);
+            panelContent.TabIndex = 1;
+            // 
+            // DashboardMainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1247, 709);
+            Controls.Add(panelContent);
+            Controls.Add(sidebarMenu);
+            MinimumSize = new Size(1263, 748);
+            Name = "DashboardMainForm";
+            Text = "NutriNyan - Dashboard";
+            Load += DashboardMainForm_Load;
+            sidebarMenu.ResumeLayout(false);
+            panelLogo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)logoApp).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel sidebarMenu;
+        private FontAwesome.Sharp.IconButton dashboardButton;
+        private Panel panelLogo;
+        private FontAwesome.Sharp.IconButton TrackingAirButton;
+        private FontAwesome.Sharp.IconButton TrackingGiziButton;
+        private FontAwesome.Sharp.IconButton settingButton;
+        private FontAwesome.Sharp.IconButton reminderButton;
+        private FontAwesome.Sharp.IconButton konsultasiButton;
+        private PictureBox logoApp;
+        private Panel panelContent;
+    }
+}
---
+++ Views/Dashboard/DashboardMainForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FontAwesome.Sharp;
+
+namespace NutriNyan.Views.Dashboard
+{
+    public partial class DashboardMainForm : Form
+    {
+        private IconButton? currentActiveButton;
+
+        public DashboardMainForm()
+        {
+            InitializeComponent();
+
+            this.WindowState = FormWindowState.Maximized;
+
+            SetActiveButton((IconButton)dashboardButton);
+            LoadDashboard();
+        }
+
+        private void DashboardMainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadUserControl(UserControl uc)
+        {
+            panelContent.Controls.Clear();
+            uc.Dock = DockStyle.Fill;
+            panelContent.Controls.Add(uc);
+        }
+
+        // Load control method
+        private void LoadDashboard()
+        {
+            LoadUserControl(new DashboardControl());
+        }
+
+        private void LoadTrackingGizi()
+        {
+            LoadUserControl(new TrackingGiziControl());
+        }
+
+        // Change bg color when btn active
+        private void SetActiveButton(IconButton clickedButton)
+        {
+            if (currentActiveButton != null)
+            {
+                currentActiveButton.BackColor = Color.Transparent;
+                currentActiveButton.ForeColor = Color.FromArgb(183, 201, 200);
+                currentActiveButton.IconColor = Color.FromArgb(183, 201, 200);
+            }
+
+            currentActiveButton = clickedButton;
+            currentActiveButton.BackColor = Color.FromArgb(0, 221, 220);
+            currentActiveButton.ForeColor = Color.FromArgb(1, 54, 63);
+            currentActiveButton.IconColor = Color.FromArgb(1, 54, 63);
+        }
+
+
+        // Button method
+        private void TrackingGiziButton_Click(object sender, EventArgs e)
+        {
+            SetActiveButton((IconButton)sender);
+            LoadTrackingGizi();
+        }
+
+        private void dashboardButton_Click(object sender, EventArgs e)
+        {
+            SetActiveButton((IconButton)sender);
+            LoadDashboard();
+        }
+    }
+}
