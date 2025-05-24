9dfb04a4532c994974009af6f2d6f56297399742
Ayuma <adityajatisari@gmail.com>
2025-05-24 17:19:57 +0700

    Adding nullable to the User Gender

--- Models/User.cs
+++ Models/User.cs
@@ -15,13 +15,13 @@ namespace NutriNyan.Models
         public string? Username { get; set; }
         public string? Password { get; set; }
         public int GenderId { get; set; }
-        public Gender? Gender { get; set; }
         public DateTime DateBirth { get; set; }
         public float Tb { get; set; }
         public float Bb { get; set; }
         public int DefaultTargetWater { get; set; }
         public required string TingkatAktivitas { get; set; }
         public int PurposeId { get; set; }
+        public Gender? Gender { get; set; }
         public Purpose? Purpose { get; set; }
 
         public DateTime CreatedAt { get; set; }
